883f579fe79a9fa3ca662069e801cfff43fa5eb5
TalatiHardik <hardikhtalati@gmail.com>
2020-07-15 11:27:11 +0530

    UC3 changes made

--- index.sh
+++ index.sh
@@ -13,7 +13,21 @@ validateName(){
 
 }
 
-checkName(){
+
+validateEmail(){
+	userEmail=$1
+	userEmailPattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+	match=0
+	if [[ $userEmail =~ $userEmailPattern ]]
+	then
+		match=1
+	fi
+	echo $match
+
+
+}
+
+checkVarialble(){
 
 	validation=$1
 	echo
@@ -26,8 +40,21 @@ checkName(){
 
 }
 
+checkEmail(){
+
+	validation=$1
+	echo
+	if [ $validation -eq 1 ]
+	then
+		echo "Entered Email is Valid"
+	else
+		echo "Entered Email is invalid"
+	fi
+
+}
+
 validation=0
-echo "Welcome to UC1 Validate Name"
+echo "Welcome to UC3 Email Validation"
 read -p "Enter UserName " userName
 validation="$( validateName $userName )"
 checkName $validation
@@ -37,5 +64,12 @@ checkName $validation
 validation=0
 echo
 read -p "Enter LastName " lastName
+validation="$( validateName $lastName )"
+checkName $validation
+
+
+validation=0
+echo
+read -p "Enter Email " lastName
 validation="$( validateName $lastName )"
 checkName $validation
