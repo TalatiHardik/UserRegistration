12d9add21ff11f463cdb09434214b64fb2b22f2b
TalatiHardik <hardikhtalati@gmail.com>
2020-07-15 10:28:16 +0530

    UC1 completed

--- index.sh
+++ index.sh
@@ -1,3 +1,27 @@
 
 #!/bin/bash -x
-echo "Welcome to master branch"
+validateName(){
+	userName=$1
+	userNamePattern="^([A-Z][a-zA-Z]){3}$"
+	match=0
+	if [[ $userName =~ $userNamePattern ]]
+	then
+		match=1
+	fi
+	echo $match
+
+
+}
+
+
+echo "Welcome to UC1 Validate Name"
+read -p "Enter UserName " userName
+validation="$( validateName $userName )"
+
+if [ $validation -eq 1 ]
+then
+	echo "User Name is Valid"
+else
+	echo "User Name invalid must have 3 characters and start with Capital"
+fi
+
